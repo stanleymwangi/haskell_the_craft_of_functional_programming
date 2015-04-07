e6cd3ce84e87e43bcb8e9c9356e6ef1f7f96a5e7
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-07 21:01:57 +0100

    Added size definition

---
+++ 01_introducing_functional_programming/05_simple_numerical_script.hs
@@ -0,0 +1,12 @@
+{- ############################################################################
+       05_simple_numerical_script.hs
+       Stanley Mwangi, April 2015
+############################################################################## -}
+
+module 05_simple_numerical_script where
+
+-- The value size is an integer (Integer), defined to be
+-- the sum of twelve and thirteen
+
+size :: Integer
+size = 12 + 13
