fa693a805eefbe484482ca2fb8d30e11dd8f5bb4
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-08 21:07:22 +0100

    Completed adding defintions of sqaure, double and example

--- 01_introducing_functional_programming/05_simple_numerical_script.hs
+++ 01_introducing_functional_programming/05_simple_numerical_script.hs
@@ -10,3 +10,18 @@ module 05_simple_numerical_script where
 
 size :: Integer
 size = 12 + 13
+
+-- The function square takes an integer
+
+square :: Integer -> Integer
+square n = n*n
+
+-- The function to double an integer
+
+double :: Integer -> Integer
+double n = 2*n
+
+-- An example using double, square and size
+
+example :: Integer
+example = double (size - square(2+2))
