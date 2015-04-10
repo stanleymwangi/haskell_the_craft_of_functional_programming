f121a7ad608e002aa36865965a7729b02dd2622f
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-10 23:38:48 +0100

    Moved and renamed file

---
+++ 02_getting_started_with_haskell/01_simple_numerical_script.hs
@@ -0,0 +1,36 @@
+{- ############################################################################
+       05_simple_numerical_script.hs
+       Stanley Mwangi, April 2015
+############################################################################## -}
+
+module 05_simple_numerical_script where
+
+-- The value size is an integer (Integer), defined to be
+-- the sum of twelve and thirteen
+
+size :: Integer
+size = 12 + 13
+
+-- The function square takes an integer
+
+square :: Integer -> Integer
+square n = n*n
+
+-- The function to double an integer
+
+double :: Integer -> Integer
+double n = 2*n
+
+-- An example using double, square and size
+
+example :: Integer
+example = double (size - square(2+2))
+
+-- The function doubles then squares the input
+doubleSquare :: Integer -> Integer
+doubleSquare n = square (2 * n)
+
+-- The function squares the input and then doubles it
+squareDouble :: Integer -> Integer
+squareDouble n = 2 * square n
+
