2caab0efd1f396e7789060e5a424f7e73f33cf5b
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-18 23:07:56 +0100

    Fixed module naming error

--- 02_getting_started_with_haskell/01_simple_numerical_script.hs
+++ 02_getting_started_with_haskell/01_simple_numerical_script.hs
@@ -3,7 +3,7 @@
        Stanley Mwangi, April 2015
 ############################################################################## -}
 
-module 05_simple_numerical_script where
+module simple_numerical_script where
 
 -- The value size is an integer (Integer), defined to be
 -- the sum of twelve and thirteen
