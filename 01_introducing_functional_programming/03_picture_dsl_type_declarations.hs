f6dddeeca83d3229acd222f48733e4edf0f25620
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-29 11:33:03 +0100

    Added example expressions using Picture dsl constructs

--- 01_introducing_functional_programming/03_picture_dsl_type_declarations.hs
+++ 01_introducing_functional_programming/03_picture_dsl_type_declarations.hs
@@ -9,3 +9,8 @@ above :: Picture -> Picture -> Picture
 beside :: Picture -> Picture -> Picture
 
 scale :: Picture -> Integer -> Picture
+
+-- example expressions using the Picture dsl
+horseChanged = horse `above` (flipH horse)
+horseBig = scale (horseChanged) (2 ^ 5)
+mirror pic = pic 'beside' (flipV pic)
